602ec41d83393a22137d42f06130e210f44adf0c
skw <dkwon1240@gmail.com>
2025-03-26 16:40:06 +0900

    header - top div (icons) layout clear

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# day5-web
-5일차 사전수업 내용입니다
-
-## input
-- git
-- github
-- markdown
-    
+hello
--- hotfix.js
+++
@@ -1,7 +0,0 @@
-const goBtn = document.querySelector("#go_btn");
-
-goBtn.addEventListener("click",goSomeWhere);
-
-function goSomeWhere() {
-    location.href = "http://youtube.com";
-}
