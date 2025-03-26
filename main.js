3a7cfc1bbb4d8b76635cba69f18a0c45297f44e6
skw <dkwon1240@gmail.com>
2025-03-26 16:53:54 +0900

    모든 저장 사항

---
+++ main.js
@@ -0,0 +1,10 @@
+/// 바로가기 메뉴(.shortcu) 마우스 닿으면(mouseeenter), 숨겨진 하위 메뉴 덩어리 표시
+const shortcu = document.querySelector(".shortcu");
+
+shortcu.addEventListener("mouseenter",showLayer);
+
+function showLayer() {
+    //숨겨둔 레이어 찾아서 표시
+    const layer1 = document.querySelector("#layer1");
+    console.log(layer1);
+}
