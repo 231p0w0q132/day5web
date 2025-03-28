7beb8da8d952e03926fddc2fa2263bd012d5c1fa
skw <dkwon1240@gmail.com>
2025-03-28 17:06:23 +0900

    헤더에 검색창 컨테이너 부분 진행중

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const shortcu = document.querySelector(".shortcu");
 
 shortcu.addEventListener("click",showLayer);
 
+window.addEventListener("dblclick",hideLayer);
+function hideLayer() {
+    const layer1 = document.querySelector("#layer1");
+    layer1.style.display ="none";
+}
 function showLayer() {
     //숨겨둔 레이어 찾아서 표시
     const layer1 = document.querySelector("#layer1");
